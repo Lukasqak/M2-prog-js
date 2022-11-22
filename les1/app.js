fca65cb8b33a0311555d2ebaf8078434bc5600af
Luke <35667@ma-web.nl>
2022-11-22 12:41:02 +0100

    html en javascript toegevoegd

---
+++ les1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        //gode gaat hier!
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+let appNaam = "cool app";
+console.log(appNaam);
+let versienummer = 0.9;
+console.log(versienummer);
+let versiedatum = "22-11-22";
+console.log(versiedatum);
+let autheur = "";
+console.log(autheur);
+let copyright = "plagiaat";
+console.log(copyright);
+let distributeur = "?";
+console.log(distributeur);
+let darkmode = True;
+console.log(darkmode);
